2fa5bc02ba906e0c4abd55bf4ba8ce97665e2c88
Eli <elipuga2@gmail.com>
2023-07-05 21:31:41 -0500

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require('./postBank.js')
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list()
+  const html = `<DOCTYPE html>
+  <html>
+    <head>
+   Wizard News
+    </head>
+    <body>
+    <ul>
+    ${posts.map( post => `<li>${post.title}</li>` ).join('') }
+    </ul>
+    </body>
+  </html>
+  `
+res.send(html)
+});
 
+app.use(morgan('dev'));
 const PORT = 1337;
 
 app.listen(PORT, () => {
